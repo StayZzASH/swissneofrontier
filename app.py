fbb8cfaa2a6d2bde846e580d99477f6e975e2015
StayZzASH <142284394+StayZzASH@users.noreply.github.com>
2026-08-24 02:41:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -165,7 +165,7 @@ else:
               formatted_msgs.append({"role": m["role"], "content": m["content"]})
 
             response = client.chat.completions.create(
-                model="llama-3.1-8b-instant",
+                model="llama-3.3-70b-versatile",
                 messages=formatted_msgs,
                 temperature=0.7,
                 presence_penalty=0.5,
